7e90b6be303bbc0d1783ffb3a9cc10de66cd8af2
Manuel Duran <mduran85@gmail.com>
2020-03-18 13:08:25 -0500

    closes #6 basic wireframe tests built

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <div className="footer">
-      <p>this is a footer</p>
+      <p>this is a test</p>
     </div>
   )
 }
