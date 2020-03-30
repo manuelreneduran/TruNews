334d9f809a714203d24077e5d587b985997c0da0
Manuel Duran <mduran85@gmail.com>
2020-03-30 12:52:48 -0500

    tests added for textarea

--- client/forms/TextArea.jsx
+++ client/forms/TextArea.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const TextArea = ({ changeHandler, postHandler }) => {
   return (
-    <>
-      <form className="submit-form flex-column">
-        <textarea onChange={(e) => changeHandler(e.target.value)}id="submit-textarea"
+    <div data-test="component-textarea">
+      <form className="submit-form flex-column" data-test="form">
+        <textarea data-test="textarea" onChange={(e) => changeHandler(e.target.value)}id="submit-textarea"
         cols={100}
         rows={30}>
         </textarea>
         <div className="submit-button-container flex-row-flex-end">
-          <button id="submit-button" onClick={(e) => postHandler(e)}>Post</button>
+          <button data-test="button" id="submit-button" onClick={(e) => postHandler(e)}>Post</button>
         </div>
       </form>
-    </>
+    </div>
   )
+}
+
+TextArea.propTypes = {
+  changeHandler: PropTypes.func,
+  postHandler: PropTypes.func
 }
 
 export default TextArea;
