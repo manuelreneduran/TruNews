36795da8246685118a9e81fafa432297504b8533
Manuel Duran <mduran85@gmail.com>
2020-04-13 12:12:25 -0500

    tested showLogin modal prop on App

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from "react-redux";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 import RegisterModal from "./RegisterModal";
@@ -8,48 +9,41 @@ import TopNews from "./TopNews";
 import MoreTopNews from "./MoreTopNews";
 import Spinner from "./Spinner";
 import hookactions from "../actions/hookactions";
-import { connect } from "react-redux";
+
 import { setUser, setLoggedIn, getData } from "../store/actions/index";
 
-export const UnconnectedApp = ({
-  showLoginModal,
-  showContactModal,
-  showRegisterModal,
-  setUser,
-  setLoggedIn,
-  articles,
-  getData,
-}) => {
-  React.useEffect(() => {
-    getData();
-  }, []);
+class UnconnectedApp extends React.Component {
 
-  React.useEffect(() => {
+  componentDidMount() {
+    this.props.getData();
     hookactions.getUserByToken(setUser, setLoggedIn);
-  }, []);
+  }
 
-  return (
-    <div data-test="container-app" id="app">
-      <NavBar data-test="navbar" />
-      {showLoginModal ? <LoginModal data-test="login-modal"/> : null}
-      {showContactModal ? <ContactModal data-test="contact-modal"/> : null}
-      {showRegisterModal ? <RegisterModal data-test="register-modal"/> : null}
-      )}
-      {articles.length > 0 ? (
-        <>
-          <h5 style={{ marginTop: "5em", margin: "70px 30px 0px 30px" }}>
-            TOP NEWS
-          </h5>
+  render() {
+    const props = this.props;
+    return (
+      <div data-test="container-app" id="app">
+        <NavBar data-test="navbar" />
+        {props.showLoginModal ? <LoginModal data-test="login-modal"/> : null}
+        {props.showContactModal ? <ContactModal data-test="contact-modal"/> : null}
+        {props.showRegisterModal ? <RegisterModal data-test="register-modal"/> : null}
+        )}
+        {props.articles.length > 0 ? (
+          <>
+            <h5 style={{ marginTop: "5em", margin: "70px 30px 0px 30px" }}>
+              TOP NEWS
+            </h5>
 
-          <TopNews data-test="top-news"topArticles={articles.slice(0, 5)} />
-          <MoreTopNews data-test="more-top-news" articles={articles.slice(5)} data-test="more-top-news" />
-        </>
-      ) : (
-        <Spinner data-test="spinner"/>
-      )}
-      <Footer data-test="footer" />
-    </div>
-  );
+            <TopNews data-test="top-news"topArticles={props.articles.slice(0, 5)} />
+            <MoreTopNews data-test="more-top-news" articles={props.articles.slice(5)} data-test="more-top-news" />
+          </>
+        ) : (
+          <Spinner data-test="spinner"/>
+        )}
+        <Footer data-test="footer" />
+      </div>
+    );
+  }
 };
 
 const mapStateToProps = (state) => {
