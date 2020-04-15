368c07c762578fab60d8ecfeca10b04aa45ec346
Manuel Duran <mduran85@gmail.com>
2020-04-15 10:06:11 -0500

    test topNews

--- client/components/TopNews.jsx
+++ client/components/TopNews.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import TopArticleJumbo from "./TopArticleJumbo";
-import TopMiniArticles from './TopMiniArticles';
+import TopMiniArticles from "./TopMiniArticles";
 
 const TopNews = ({ topArticles }) => {
   const topMiniArticles = topArticles.slice(1);
   return (
-    <Container className="mb-3" fluid>
-      <Row>
-
-      </Row>
+    <Container data-test="component-top-news" className="mb-3" fluid>
       <Row>
         <Col lg={6}>
           <TopArticleJumbo
+            data-test="top-article-jumbo"
             urlToImage={topArticles[0].urlToImage}
             url={topArticles[0].url}
             content={topArticles[0].content}
@@ -23,7 +21,10 @@ const TopNews = ({ topArticles }) => {
           />
         </Col>
         <Col lg={6} className="d-none d-lg-block">
-          <TopMiniArticles topMiniArticles={topMiniArticles}/>
+          <TopMiniArticles
+            data-test="top-mini-articles"
+            topMiniArticles={topMiniArticles}
+          />
         </Col>
       </Row>
     </Container>
